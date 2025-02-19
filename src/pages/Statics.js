cacf14724c1176893e4d052fc2bdcfbb480ec0c1
dong279 <dongho0130@daum.net>
2025-02-19 17:09:13 +0900

    front2/19

--- src/pages/Statics.js
+++ src/pages/Statics.js
@@ -5,40 +5,23 @@ import { Progress, Card, Row, Col } from "antd";
 import "../component/Css/Statics.css";
 
 export const Statics = () => {
-  const percentages = [
-    { value: 80 },
-    { value: 60 },
-    { value: 50 },
-    { value: 90 },
-    { value: 80 },
-    { value: 60 },
-    { value: 50 },
-    { value: 90 },
-    { value: 80 },
-    { value: 60 },
-    { value: 50 },
-    { value: 90 },
+  const tasks = [
+    { name: "쿠팡", value: 80 },
+    { name: "11번가", value: 60 },
+    { name: "알리익스프레스", value: 50 },
+    { name: "G마켓", value: 90 },
+    { name: "e마트", value: 90 },
   ];
 
   return (
-    <div>
+    <div className="staticmain">
       <Header />
       <div className="progress-container">
         <Row gutter={[16, 16]}>
-          {percentages.map((percent, index) => (
+          {tasks.map((task, index) => (
             <Col span={8} key={index}>
-              <Card title={`Task ${index + 1}`} bordered={false}>
-                <Progress
-                  percent={percent.value}
-                  status={
-                    percent.value === 100
-                      ? "success"
-                      : percent.value >= 50
-                        ? "active"
-                        : "exception"
-                  }
-                  showInfo={true}
-                />
+              <Card className="static_card" title={task.name} bordered={false}>
+                <Progress percent={task.value} showInfo={true} />
               </Card>
             </Col>
           ))}
